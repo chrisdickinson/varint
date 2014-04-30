430379c346eaa67bf685f968b91969ffabf6d9bf
Mathias Buus <mathiasbuus@gmail.com>
2014-04-30 21:12:13 +0200

    add encode.bytesWritten

--- encode.js
+++ encode.js
@@ -7,11 +7,15 @@ var MSB = 0x80
 function encode(num, out, offset) {
   out = out || []
   offset = offset || 0
+  var oldOffset = offset
 
   while(num & MSBALL) {
     out[offset++] = (num & 0xFF) | MSB 
     num >>>= 7
   }
   out[offset] = num
+  
+  encode.bytesWritten = offset - oldOffset + 1
+  
   return out
 }
