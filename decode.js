3f82763190ee06acd18ee152ac546571a70ab003
Timothy Bradley <fennasnogothrim@gmail.com>
2021-11-21 17:12:07 -0700

    Make decode almost as efficient as encode

--- decode.js
+++ decode.js
@@ -2,25 +2,24 @@ module.exports = read
 
 var MSB = 0x80
   , REST = 0x7F
+  , TWO_POWER_SEVEN = Math.pow(2, 7)
 
 function read(buf, offset) {
   var res    = 0
     , offset = offset || 0
-    , shift  = 0
+    , shift  = 1
     , counter = offset
     , b
-    , l = buf.length
+    , l = Math.pow(TWO_POWER_SEVEN, buf.length - offset < 8 ? (buf.length - offset) * 7 : 49)
 
   do {
-    if (counter >= l || shift > 49) {
+    if (shift > l) {
       read.bytes = 0
       throw new RangeError('Could not decode varint')
     }
     b = buf[counter++]
-    res += shift < 28
-      ? (b & REST) << shift
-      : (b & REST) * Math.pow(2, shift)
-    shift += 7
+    res += (b & REST) * shift
+    shift = shift * TWO_POWER_SEVEN
   } while (b >= MSB)
 
   read.bytes = counter - offset
